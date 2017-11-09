7885ce952b72f50e78918e924fbe0423c97ce8ca
SecretMNC <pettkevinj@gmail.com>
2017-11-09 12:01:16 -0700

    bugs

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use( express.static( `${__dirname}/../build` ) );
+
 massive(process.env.CONNECTION_STRING).then(db => {
     app.set('db', db)
 });
@@ -73,6 +75,11 @@ app.get('/api/bios/', (req, res) => {
         res.status(200).send(response)
     })
 });
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
